5683cdeef5ba64c2c8fa31f39a8edce1cf8abb0c
SERGEY KIM <serkim1209@gmail.com>
2025-11-30 20:27:20 +0900

    закончил свайпер

---
+++ script.js
@@ -0,0 +1,18 @@
+new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 60,
+  slidesPerView:1.5,
+  centeredSlides: false,
+
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+
+  } 
+
+
+});
